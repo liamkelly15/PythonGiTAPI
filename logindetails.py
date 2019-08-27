c58f0e3fe333ac95880f08cb71c7afb3e43cf182
Liam Kelly <47215803+liamkelly15@users.noreply.github.com>
2019-08-27 17:29:00 +0100

    Update logindetails.py

--- logindetails.py
+++ logindetails.py
@@ -1,4 +1,4 @@
-user='f33kv0p'
+user='*******'
 token='9qGspXJCxZr-qxqsswvD'
 ProjectList="/home/f33kv0p/fdn-gitlab-management/fdn-gitlab-management/ProjectList.json"
 baseAttributes="/home/f33kv0p/fdn-gitlab-management/fdn-gitlab-management/baseAttributesList.json"
